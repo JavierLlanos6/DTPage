4459cc2e14147ec966c6ca0a08c6e650bc1f5f5c
JavierLlanos6 <xavi6poop@gmail.com>
2025-06-11 16:45:16 -0400

    added styles historymatches

--- src/pages/StrategyPage.jsx
+++ src/pages/StrategyPage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { db } from "../services/firebase";
 import { collection, getDocs } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
+import "../css/StrategyPage.css";
 
 export default function StrategyPage() {
   const [matches, setMatches] = useState([]);
@@ -20,17 +21,17 @@ export default function StrategyPage() {
   }, []);
 
   return (
-    <div className="p-4">
-      <h2 className="text-2xl font-bold mb-4">Partidos guardados</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div className="strategy-container">
+      <h2 className="strategy-title">Partidos guardados</h2>
+      <div className="strategy-grid">
         {matches.map((match) => (
           <div
             key={match.id}
-            className="bg-white shadow-md rounded-xl p-4 cursor-pointer hover:scale-105 transition"
+            className="strategy-card"
             onClick={() => navigate(`/estrategias/${match.id}`)}
           >
-            <h3 className="text-xl font-semibold">{match.opponent}</h3>
-            <p className="text-gray-500">{match.id}</p>
+            <h3>{match.opponent}</h3>
+            <p>{match.id}</p>
           </div>
         ))}
       </div>
