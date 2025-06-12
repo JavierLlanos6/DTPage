8f7e41f2c2700a6bd0c1f43ffaf8ec3d4a3ba53a
JavierLlanos6 <xavi6poop@gmail.com>
2025-06-12 16:02:30 -0400

    fixed error in the handlefieldclick

--- src/pages/MatchDataPage.jsx
+++ src/pages/MatchDataPage.jsx
@@ -69,7 +69,7 @@ export default function MatchDataPage() {
 
   // Manejo de eventos en campo (solo para local)
   const handleFieldClick = (e) => {
-    const rect = e.target.getBoundingClientRect();
+    const rect = e.currentTarget.getBoundingClientRect();
     const x = ((e.clientX - rect.left) / rect.width) * 100;
     const y = ((e.clientY - rect.top) / rect.height) * 100;
     const type = prompt("Tipo de evento (goal, shot, conceded, shotconceded):");
